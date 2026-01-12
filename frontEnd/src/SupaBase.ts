1f47596263c3449272cfc6ab4002dd594f48a46d
MuhammadS1364 <shafeealam1364@outlook.com>
2026-01-12 15:29:07 +0530

    SupaBase Connection SeccessFull

--- frontEnd/src/SupaBase.ts
+++ frontEnd/src/SupaBase.ts
@@ -2,6 +2,13 @@
 
 
 // Firstly Import createClient from supabase
-// Second Create Url for the Project 
+// Second Create Url for the Project Id
 // Third Create the Anomy Key (publicabel Key)
+import { createClient } from "@supabase/supabase-js";
+const SupaBaseURL = 'https://wtskwbdxlnguapeqipvr.supabase.co';
 
+const SupaBaseKey = 'sb_publishable_x2L2KPTJhhsUCsaPplhV_g_V_Dh6P7V';
+
+// create the Client 
+
+export const MySupaBase = createClient(SupaBaseURL, SupaBaseKey)
