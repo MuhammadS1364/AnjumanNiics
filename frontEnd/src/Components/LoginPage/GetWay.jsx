381fa3983b0493ac596b630aac0b7a7b70f0539f
MuhammadS1364 <shafeealam1364@outlook.com>
2026-01-12 11:48:43 +0530

    Button Input and Ul added

---
+++ frontEnd/src/Components/LoginPage/GetWay.jsx
@@ -0,0 +1,9 @@
+
+import FormInput from "../Forms/FormLabel";
+import ButtonEle from "../Buttons/Button";
+
+export default function GetWay(){
+    return(
+        
+    )
+}
---
+++ frontEnd/src/Components/OrderList/OrderList.jsx
@@ -0,0 +1,17 @@
+
+
+export default function OrderList({names}){
+    return (
+        <div className="order-list">
+            <ol>
+            {names.map((name, index) =>{
+                return(
+                    <li key={index}>{name}</li>
+                )
+            })}   
+            </ol>
+
+        </div>
+    )
+}
+
