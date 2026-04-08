47cc049e99a29c3b24af5194becfb1ecb3947610
MuhammadS1364 <shafeealam1364@outlook.com>
2026-04-08 22:09:39 +0530

    Router Setup

---
+++ ncms-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+export default function App (){
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
